91d65abe9ac86810e0dfe57d39c7000d0f3920cd
Luis Juarros <juarrosl0806@gmail.com>
2020-06-06 00:41:14 -0700

    Express REST API with MongoDB and Mongoose Part 1

---
+++ models/campsite.js
@@ -0,0 +1,69 @@
+//schema build
+const mongoose = require('mongoose');
+//not necessary. just shorthand to use Schema not all
+const Schema = mongoose.Schema;
+//new currency type schema for db to use
+require('mongoose-currency').loadType(mongoose);
+//shorthand
+const Currency = mongoose.Types.Currency;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const campsiteSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    elevation: {
+        type: Number,
+        required: true
+    },
+    cost: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+//new Model searches the collection to be Capitalized and singular
+//we also pass the schema for the model to use.
+//returns a constructor function
+//Models are de-sugared classes method instantiated
+const Campsite = mongoose.model('Campsite', campsiteSchema);
+
+
+module.exports = Campsite;
+
+
--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -2,6 +2,10 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+//bring in Campsite.js file
+const Campsite = require('../models/campsite');
+
+
 
 const campsiteRouter = express.Router();
 
@@ -10,34 +14,55 @@ campsiteRouter.use(bodyParser.json());
 
 
 //catches all for all types of HTTP requests to this route. It passes information to all the .get .post? *share same path.. chained now without APP or semicolon or path.
+
+//deleted .all so we do status codes and headers for various endpoints
 campsiteRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+
+.get((req, res, next) => {
+    Campsite.find()
+    .then(campsites => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsites);
+    })
+    .catch(err => next(err));
+    //removed res.end with the catch, it lets express handle error
 })
-.get((req, res) => {
-    res.end('Will send all the campsites to you');
-})
-.post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+.post((req, res, next) => {
+    //posts from what the client put in parsed
+    //Campsite will check the schema to match all predefined rules
+    Campsite.create(req.body)
+    .then(campsite => {
+        console.log('Campsite Created', campsite);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err));
 })
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /campsites');
 })
-.delete((req, res) => {
-    res.end('Deleting all campsites');
+.delete((req, res, next) => {
+    Campsite.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
 campsiteRouter.route('/:campsiteId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+.get((req, res, next) => {
+    Campsite.findById(req.params.campsiteId)
+    .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err));
 })
 
 .post((req, res) => {
@@ -45,14 +70,26 @@ campsiteRouter.route('/:campsiteId')
     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
 })
 
-.put((req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`)
-    res.end(`Will update the campsite: ${req.body.name}
-    with description: ${req.body.description}`);
-})
+.put((req, res, next) => {  
+    Campsite.findByIdAndUpdate(req.params.campsiteId, {
+        $set: req.body
+    }, { new: true })
+    .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+    })
+    .catch(err => next(err));
+    })
 
-.delete((req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+.delete((req, res, next) => {
+    Campsite.findByIdAndDelete(req.params.campsiteId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
 
