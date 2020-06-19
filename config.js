9617f913ea10c1a9882836e80843c9fbc9cbbc7c
P0l4ris <60461233+P0l4ris@users.noreply.github.com>
2020-06-18 18:17:18 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '405923463682666',
-        clientSecret: 'd0c0a226dc296b783585b1b09e8cb80c'
-    }
-}
